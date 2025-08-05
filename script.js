f393f5cec2542eafc6546354da959b251e01eb18
Helene Delachapelle Sdez <helene.delachapelle@edhec.com>
2025-08-05 14:02:37 +0200

    feat: added a slide out effect to the modal/div-color

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-// $(document).ready(function () {
-//   var colors = ["#6699cc", "#02394a", "#ff4b3e", "#fcd3de", "#ffe66d", "#d00000"];
-//   var new_color = colors[Math.floor(Math.random() * colors.length)];
-//   $("#color-div").css("background-color", new_color);
-// });
+const palette = [["#6699cc"], ["#02394a"], ["#ff4b3e"], ["#fcd3de"], ["#ffe66d"], ["#d00000"]];
+const randomPalette = palette[Math.floor(Math.random() * palette.length)];
+let bgColor = randomPalette[0];
 
-window.onload = () => {
-  const palette = [["#6699cc"], ["#02394a"], ["#ff4b3e"], ["#fcd3de"], ["#ffe66d"], ["#d00000"]];
-  const randomPalette = palette[Math.floor(Math.random() * palette.length)];
-  let bgColor = randomPalette[0];
+const main = document.getElementById("color-div").style;
+main.backgroundColor = bgColor;
 
-  const main = document.getElementById("color-div").style;
-  main.backgroundColor = bgColor;
-};
+function toggleColorDiv() {
+  const colorDiv = document.getElementById("color-div");
+  const modalSeen = localStorage.getItem("modalSeen");
+
+  if (modalSeen) {
+    colorDiv.style.display = "none";
+  } else {
+    colorDiv.style.display = "flex";
+    localStorage.setItem("modalSeen", true);
+  }
+
+  setTimeout(() => {
+    colorDiv.classList.add("slide-up-out");
+
+    // After animation, fully hide the element
+    setTimeout(() => {
+      colorDiv.style.display = "none";
+      colorDiv.classList.remove("slide-up-out"); // Reset for future use
+    }, 500);
+  }, 5000);
+}
+
+toggleColorDiv();
